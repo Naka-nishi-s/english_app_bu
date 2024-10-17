9075cb41cef46b00b816d8eda8eb801b52ee6b29
Hyodo <90432430+Naka-nishi-s@users.noreply.github.com>
2024-10-17 16:00:34 +0900

    first_commit

---
+++ app/page.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+export default function Home() {
+  /**
+   * 認証後、Topページに遷移
+   */
+  const navigateToTop = () => {};
+
+  return (
+    <main>
+      <div>
+        <p>sa</p>
+        <button onClick={navigateToTop}>スタート</button>
+      </div>
+    </main>
+  );
+}
